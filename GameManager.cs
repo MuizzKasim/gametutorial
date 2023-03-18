9c4c66c72c3e264b01fc65707de7c28c121ebe5e
Muizz Kasim <125282326+MuizzKasim@users.noreply.github.com>
2023-03-18 20:49:36 +0800

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public int score;
+	public Text scoreDisplay;
+	public Text endGame;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		SetScore ();
+		endGame.text = "";
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+		
+	public void SetScore(){
+
+		scoreDisplay.text = "Score: " + score.ToString ();
+		if(score>=3){
+			endGame.text = "Game Over";
+		}
+
+	}
+}
---
+++ MoveScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+	public float speed = 10.0f;
+	private Rigidbody rb;
+	private GameManager gm;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+		GameObject theScore = GameObject.Find ("GameManager");
+		gm = theScore.GetComponent <GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+		rb.AddForce(movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Pick Up"){
+			other.gameObject.SetActive (false);
+
+			gm.score = gm.score + 1;
+			gm.SetScore ();
+		}
+	}
+}
